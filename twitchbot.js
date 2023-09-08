61728fb6a2d89fb8ed6a7c7f154a7ef5f97a7e08
Tzyber <134324777+Tzyber@users.noreply.github.com>
2023-09-08 18:46:36 +0200

    added subcriber function

--- twitchbot.js
+++ twitchbot.js
@@ -3,24 +3,35 @@ const express = require('express');
 const axios = require('axios');
 const {spawn} = require('child_process');
 const natural = require('natural');
-
+const fs = require('fs');
 const app = express();
 
 // Twitch-Konfiguration
-const clientID = 'client-id';
-const clientSecret = 'client secret ';
+const clientID = 'clientid';
+const clientSecret = 'clientsecret';
 const redirectURI = 'http://localhost:3000/callback';
 const scopes = ['chat:read', 'chat:edit'];
+let lastGiftedUser = '';
+let giftCount = 0;
 const channel = 'channel';
-const wordsToGuess = ['Blume', 'Hund', 'Katze', 'Tisch', 'Stuhl', 'Haus', 'Auto', 'Baum', 'Buch', 'Schule', 'Computer', 'Handy', 'Kaffee', 'Pizza', 'Musik', 'Sonne', 'Mond', 'Berge', 'Meer', 'Stern'];
-
+const giftedUsers = new Set();
+let quizStarted = false;
+const wordsToGuess = [
+    "wordstoGuessArray,"
+];
 let twitchClient; // Twitch-Client-Instanz
 let currentWordIndex = 0; // Index des aktuellen zu erratenden Wortes
 
 // Routen für den oauth
 app.get('/', (req, res) => {
     const authorizeURL = `https://id.twitch.tv/oauth2/authorize?client_id=${clientID}&redirect_uri=${encodeURIComponent(redirectURI)}&response_type=code&scope=${scopes.join(' ')}`;
-    spawn(/^win/.test(process.platform) ? 'explorer' : 'xdg-open', [authorizeURL]);
+
+    if (/^win/.test(process.platform)) {
+        spawn('explorer', [authorizeURL]);
+    } else {
+        spawn('xdg-open', [authorizeURL]);
+    }
+
     res.send('Öffne deinen Browser und melde dich bei Twitch an.');
 });
 
@@ -48,14 +59,27 @@ app.get('/callback', async (req, res) => {
                 reconnect: true
             },
             identity: {
-                username: 'BeensQuizBot',
+                username: 'twitchBot',
                 password: `oauth:${accessToken}`
             },
             channels: [channel]
         });
 
+
+        // Funktion zum Schreiben der Chatnachrichten in die Datei "chatlog.txt"
+        function writeChatMessageToFile(tags, message) {
+            const username = tags.username;
+            const timestamp = new Date().toLocaleString();
+            const logEntry = `[${timestamp}] ${username}: ${message}\n`;
+            let currentDate = new Date().toJSON().slice(0.10)
+            fs.appendFile(currentDate, logEntry, (err) => {
+                if (err) {
+                    console.error('Fehler beim Schreiben der Chatnachricht in die Datei:', err);
+                }
+            });
+        }
+
         // Funktion zur Berechnung der Ähnlichkeit von Wörtern
-        //natural language processing
         function calculateSimilarity(word1, word2) {
             const lowercaseWord1 = word1.toLowerCase();
             const lowercaseWord2 = word2.toLowerCase();
@@ -63,6 +87,7 @@ app.get('/callback', async (req, res) => {
             if (lowercaseWord1 === lowercaseWord2) {
                 return 1;
             }
+
 
             const length1 = lowercaseWord1.length;
             const length2 = lowercaseWord2.length;
@@ -80,25 +105,60 @@ app.get('/callback', async (req, res) => {
             // Überprüfen, ob das richtige Wort geschrieben wurde (mit Rechtschreibfehlerprüfung)
             const targetWord = wordsToGuess[currentWordIndex];
             const similarity = calculateSimilarity(targetWord, message);
-            console.log(similarity)
-            if (similarity > 0.8) {
-                // Nachricht im Chat senden
-                const username = tags.username;
-                twitchClient.say(channel, `Das richtige Wort wurde von ${username} geschrieben: '${message}' das Gesuchte Wort war: ${targetWord}`);
+            const username = tags.username;
 
-                // Increment  aktuellen Wortindex.
-                currentWordIndex++;
-
-                // Prüfen ob schon alle Wörter geraten wurden
-                if (currentWordIndex === wordsToGuess.length) {
-                    // Disconnect wenn alle Wörter geraten wurden
-                    twitchClient.disconnect();
-                }
+            if (!quizStarted && message.toLowerCase() === '!startquiz') {
+                quizStarted = true; // Quiz wird gestartet
+                twitchClient.say(channel, 'Quiz wurde gestartet! Viel Spaß beim Raten!');
+                return; // Beende die Funktion, um keine weiteren Nachrichten zu verarbeiten
             }
+
+
+                if (similarity > 0.8 && quizStarted) {
+                    console.log(similarity, username + ": ", message)
+                    const username = tags.username;
+                    twitchClient.say(channel, `Das richtige Wort wurde von ${username} geschrieben: "${message}" das Gesuchte Wort war: ${targetWord}`);
+
+                    // Increment  aktuellen Wortindex.
+                    currentWordIndex++;
+
+                    // Prüfen ob schon alle Wörter geraten wurden
+                    if (currentWordIndex === wordsToGuess.length) {
+                        // Disconnect wenn alle Wörter geraten wurden
+                        twitchClient.disconnect();
+                    }
+                }
+
+
         }
 
         // Twitch-Client-Event: Nachricht erhalten
         twitchClient.on('message', handleMessage);
+        // Twitch-Client-Event: Subscriptions
+        twitchClient.on('subscription', (channel, username, method, message, userstate) => {
+            twitchClient.say(channel,`${username} Danke für den Sub! beeentVdance `);
+        });
+
+        twitchClient.on('subgift', (channel, username, streakMonths, recipient, methods, userstate) => {
+            giftedUsers.add(recipient);
+
+            if (username === lastGiftedUser) {
+                giftCount++;
+            } else {
+                lastGiftedUser = username;
+                giftCount = 1;
+            }
+
+            if (giftCount === 1) {
+                twitchClient.say(channel, `${username} danke für ${giftedUsers.size} verschenkte Sub(s)  beeentVdance`);
+            }
+        });
+
+        // Twitch-Client-Event: Resubscriptions
+        twitchClient.on('resub', (channel, username, months, message, userstate, methods) => {
+            const cumulativeMonths = userstate['msg-param-cumulative-months'];
+            twitchClient.say(channel, `${username} Danke für den Sub seit ${cumulativeMonths} Monat(e). ! beeentVdance `)
+        });
 
         // Twitch-Client starten
         twitchClient.connect().catch(console.error);
